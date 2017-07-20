56a3b9166fc71aec07998865769f1456d227ebd9
WatcharaChe <Watchara.che@gmail.com>
2017-07-20 08:37:09 +0700

    แก้ไข case ที่ 1,2,3,4,5,6 ผ่าน

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,8 +1,8 @@
 public class FizzBuzz {
     public String say(int input) {
-        if (input == 3) {
+        if (input == 3 || input == 6) {
             return "Fizz";
-        }else if (input == 5){
+        } else if (input == 5) {
             return "Buzz";
         }
         return String.valueOf(input);
