0378a7d5ef5c32f01c9bb30ed6f14595f6377b32
WatcharaChe <Watchara.che@gmail.com>
2017-07-20 08:45:08 +0700

    แก้ไข case ที่ 1-13 ผ่าน

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz {
     public String say(int input) {
         if (input == 3 || input == 6 || input == 9) {
             return "Fizz";
-        } else if (input == 5 || input == 10) {
+        } else if (input == 5 || input == 10 || input == 20) {
             return "Buzz";
         } else if (input == 15) {
             return "FizzBuzz";
