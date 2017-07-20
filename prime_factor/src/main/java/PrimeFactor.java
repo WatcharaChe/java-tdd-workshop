a5a5152bea493e86d1b24382e837cb0a0cbca660
WatcharaChe <Watchara.che@gmail.com>
2017-07-20 14:57:44 +0700

    improve code ครั้งที่ 1 ให้ผ่าน case ที่ 1-8 ผ่าน Project primeFactor

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,31 +4,40 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> list = new ArrayList<Integer>();
-        if (number == 1) {
-        } else if (number  == 4) {
-            list.add(2);
-            list.add(2);
 
-        } else if(number == 6){
-            list.add(2);
-            list.add(3);
-        }else if (number == 8){
-            list.add(2);
-            list.add(2);
-            list.add(2);
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                list.add(i);
+                number /= i;
+            }
         }
-        else if (number == 9){
-            list.add(3);
-            list.add(3);
-        }
-        else if (number == 27){
-            list.add(3);
-            list.add(3);
-            list.add(3);
-        }
-
-        else
-            list.add(number);
         return list;
+//
+//        if (number == 1) {
+//        } else if (number  == 4) {
+//            list.add(2);
+//            list.add(2);
+//
+//        } else if(number == 6){
+//            list.add(2);
+//            list.add(3);
+//        }else if (number == 8){
+//            list.add(2);
+//            list.add(2);
+//            list.add(2);
+//        }
+//        else if (number == 9){
+//            list.add(3);
+//            list.add(3);
+//        }
+//        else if (number == 27){
+//            list.add(3);
+//            list.add(3);
+//            list.add(3);
+//        }
+//
+//        else
+//            list.add(number);
+//        return list;
     }
 }
