4375568bbd89a0c204177719b28f457d1ec9d8de
WatcharaChe <Watchara.che@gmail.com>
2017-07-20 15:22:40 +0700

    แก้ไข case ที่ 1-3 ของ Project Format number phone ผ่าน

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,11 +1,17 @@
 public class FormatPhoneNumber {
 
     public String removeNonDigit(String input) {
-        return input.replaceAll("-| ","");
+        return input.replaceAll("-| ", "");
     }
 
     public String solution(String input) {
-        return null;
+        String formatPhoneNumber[] = input.split("(?<=\\G...)");
+        int length = formatPhoneNumber.length;
+        String ans = "";
+        for (int i = 0; i < length; i++) {
+            ans += formatPhoneNumber[i] + "-";
+        }
+        return ans.substring(0,ans.length()-1);
     }
 
 }
