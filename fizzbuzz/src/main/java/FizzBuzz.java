99ff6008588a9b0213f1bc1eb78ce9d7575b52cc
WatcharaChe <Watchara.che@gmail.com>
2017-07-20 09:21:50 +0700

    improve code ครั้งที่ 2 ให้ผ่าน case ที่ 1-14 ผ่าน

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,6 +1,10 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
     public String say(int input) {
-        if (input % 3 == 0 && input % 5 == 0) {
+
+        if (input % 15 == 0) {
             return "FizzBuzz";
         } else if (input % 3 == 0) {
             return "Fizz";
